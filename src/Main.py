a324e287a3b089389122b366a5d5ec1cd12ebdae
Jaydeep Singh <jaydeep.singh.424@gmail.com>
2015-12-21 17:59:47 -0600

    Main.py checks to see if a post is a youtube video and contains 1 or more keywords. If it does, it returns a list of all youtube videos associated with the given keyword.

--- src/Main.py
+++ src/Main.py
@@ -2,8 +2,6 @@ import praw
 import time
 r = praw.Reddit("ProPlayerInfo v 1.0")
 keywords = set(["bjergson","dyrus", "talon"])
-for word in keywords:
-    print(word)
 #r.login("ProPlayerInfoBot", "MojoJojoBridgeUnderTroubledWater")
 already_visited = set()
 run_count = 0
@@ -12,17 +10,26 @@ while True:
     run_count += 1
     submissions = r.get_subreddit("leagueoflegends").get_hot(limit=5)
     for submission in submissions:
-        text = submission.title.lower()
+        #check if video
         domain = submission.domain
-        is_title = domain == "youtube.com"
+        is_title = (domain == "youtube.com" or domain == "youtu.be" or domain == "twitch.tv" or domain == "oddshot.tv")
+        body_text = str.split(submission.selftext.lower())
+        for word in body_text:
+            if word.__contains__("oddshot") or word.__contains__("youtu.be") or word.__contains__("twitch.tv") or word.__contains__("youtube"):
+                is_title = True
+
+        #check for keyword
+        text = submission.title.lower()
         list_of_words = str.split(text)
         contains_keyword = False
         for word in list_of_words:
             if keywords.__contains__(word):
                 containsKeyword = True
+
+        #return related videos here
         if not already_visited.__contains__(submission.id) and contains_keyword and is_title:
             already_visited.add(submission.id)
-            print("SUCCESS!")
+
     time.sleep(1800)
 
 
