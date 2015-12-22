71d8517d2583d291c5ea4cdd06f9592d48a00a32
Jaydeep Singh <jaydeep.singh.424@gmail.com>
2015-12-21 18:28:38 -0600

    Sends a message to Jaydeep. Replace this with commenting on correct posts once the database is built.

--- src/Main.py
+++ src/Main.py
@@ -1,22 +1,23 @@
 import praw
 import time
 r = praw.Reddit("ProPlayerInfo v 1.0")
-keywords = set(["bjergson","dyrus", "talon"])
-#r.login("ProPlayerInfoBot", "MojoJojoBridgeUnderTroubledWater")
+keywords = set(["bjergson","dyrus", "talon", "korean"])
+
+r.login("ProPlayerInfoBot", "MojoJojoBridgeUnderTroubledWater", disable_warning=True)
 already_visited = set()
 run_count = 0
 while True:
     print("Running " + run_count.__str__())
     run_count += 1
-    submissions = r.get_subreddit("leagueoflegends").get_hot(limit=5)
+    submissions = r.get_subreddit("leagueoflegends").get_hot(limit=25)
     for submission in submissions:
         #check if video
         domain = submission.domain
-        is_title = (domain == "youtube.com" or domain == "youtu.be" or domain == "twitch.tv" or domain == "oddshot.tv")
-        body_text = str.split(submission.selftext.lower())
-        for word in body_text:
-            if word.__contains__("oddshot") or word.__contains__("youtu.be") or word.__contains__("twitch.tv") or word.__contains__("youtube"):
+        is_title = ((domain == "youtube.com") or (domain == "youtu.be") or (domain == "twitch.tv") or (domain == "oddshot.tv"))
+        word = submission.selftext.lower()
+        if word.__contains__("oddshot") or word.__contains__("youtu.be") or word.__contains__("twitch.tv") or word.__contains__("youtube"):
                 is_title = True
+
 
         #check for keyword
         text = submission.title.lower()
@@ -24,12 +25,13 @@ while True:
         contains_keyword = False
         for word in list_of_words:
             if keywords.__contains__(word):
-                containsKeyword = True
+                contains_keyword = True
 
         #return related videos here
         if not already_visited.__contains__(submission.id) and contains_keyword and is_title:
             already_visited.add(submission.id)
-
+            print("sent message")
+            r.send_message("jaydeep24", "test_message", submission.title)
     time.sleep(1800)
 
 
