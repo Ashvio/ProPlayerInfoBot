bbde328e6184f7cfd53420cc6006f3234277d365
Ashvio <ashvion@gmail.com>
2015-12-22 17:46:15 -0600

    Updated to include region, removed some comments

--- src/ParseInfoFromWiki.py
+++ src/ParseInfoFromWiki.py
@@ -9,7 +9,7 @@ from src.Player import Player
 # Returns a list of dictionary mapping player names to players, found in the file
 
 
-def build_database(filename):
+def build_database(filename, region):
     html_file = open(filename, "r", encoding="UTF-8")
     table_string = html_file.read()
     parser = etree.XMLParser(recover=True)
@@ -19,9 +19,6 @@ def build_database(filename):
     table.xpath('//tr/td//text()')
 
     rows = iter(table)
-
-    # Set the headers as the strings in the first column
-    headers = [col for col in next(rows)]
 
     player_dict = {}
     for row in rows:
@@ -46,16 +43,12 @@ def build_database(filename):
                     player.stream_site = "Azubu"
                 elif "twitch" in player.stream_link.lower():
                     player.stream_site = "Twitch"
-            # print(col.text)
-            # if col.get("class") is not None:
-            #     print(col.get("class"))
 
-            # values = [col.text for col in row]
-            # print(values)
-            # print(dict(zip(headers, values)))
+            # put the player in the dictionary
+            player.region = region
             player_dict[name[1:]] = player
 
     return player_dict
 
 
-player_list = build_database("../NALCS.html")
+player_list = build_database("../NALCS.html", "NA")
